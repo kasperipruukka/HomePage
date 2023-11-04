aae0ba295dba2c1f78ff5134516a20f37aa95bff
Kasperi Pruukka <55744887+kasperipruukka@users.noreply.github.com>
2023-11-04 17:56:26 +0200

    Muuteltu tyylityksiä. Lisätty ikä-päättely.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,47 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1000, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  });
+window.addEventListener('load', function() {
+  setAge('age');
+});
+
+function setAge(id) {
+  const element = document.getElementById(id);
+  if (!element) {
+    logMessage('Age could not be set.');
+    return;
+  }
+
+  const age = getAge('1997-09-26');
+  element.textContent = age;
+}
+
+function getAge(birthDate) {
+  const ageFallbackValue = 26;
+
+  const birthYear = getBirthYear(birthDate);
+  if (!birthYear) {
+    return ageFallbackValue;
+  }
+
+  const age = new Date().getFullYear() - birthYear;
+  if (!age || isNaN(age)) {
+    return ageFallbackValue;
+  }
+
+  return age;
+}
+
+function getBirthYear(birthDate) {
+  if (!birthDate) return;
+
+  const birthYear = new Date(birthDate).getFullYear();
+  return birthYear;
+}
+
+function logMessage(message) {
+  console.log(message);
+}
+
+function isValidDate(date) {
+  if (!date) return false;
+  const jsDate = new Date(date);
+  return !isNaN(jsDate.getTime());
+}
